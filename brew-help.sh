1f184336bb9dcccf1a33db1ab1a51d9c5f3d2552
baumannalexj <baumannalexj@gmail.com>
2018-02-11 17:35:47 -0600

    created brew-help.sh

---
+++ brew-help.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# this script runs your brew command on a batch of brew packages
+# ex: `$ ./brew-help.sh "brew cask install" brew-cask-list.txt`
+
+commandToExecute="$1"
+echo commandToExecute
+filename="$2"
+echo filename
+
+while read -r line
+do
+
+	{
+	    brewFormulaName=$line
+	    echo "trying $brewFormulaName"
+
+	    eval "$commandToExecute $brewFormulaName"
+	    echo "finished $brewFormulaName"
+   	} & 
+   	# end with '&' to run block in background (parallelizes)
+
+done < "$filename"
+
+wait
+
